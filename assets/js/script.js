90f2e04deafe16b9230eb5d2dbb92d52163c19c0
Ernesto Valecillos <evalecillos@gmail.com>
2021-08-28 13:15:52 -0500

    fixed shuffle of final variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ function generatePassword() {
 // ask user if lowercase
   var confLower = confirm("Would you like to include lowercase characters to your password?");
   if (confLower) {
-    confirm("Are you sure you'd like to add lowercase characters??");
+    // confirm("Are you sure you'd like to add lowercase characters??");
     userPassPool = userPassPool + lowerC;
     tempPass += lowerC[Math.floor(Math.random() * lowerC.length)];
     console.log(tempPass);
@@ -25,7 +25,7 @@ function generatePassword() {
 //ask user if uppercase
   var confUpper = confirm("Would you like to include UPPERCASE characters to your password?");
   if (confUpper) {
-    confirm("Are you sure you'd like to add UPPERCASE characters??");
+    // confirm("Are you sure you'd like to add UPPERCASE characters??");
     userPassPool = userPassPool + upperC;
     tempPass += upperC[Math.floor(Math.random() * upperC.length)];
     console.log(tempPass);
@@ -33,7 +33,7 @@ function generatePassword() {
   //ask user if numbers
   var confNume = confirm("Would you like to include num3r1c characters to your password?");
   if (confNume) {
-    confirm("Are you sure you'd like to add num3r1c characters??");
+    // confirm("Are you sure you'd like to add num3r1c characters??");
     userPassPool = userPassPool + numb;
     tempPass += numb[Math.floor(Math.random() * numb.length)];
     console.log(tempPass);
@@ -41,7 +41,7 @@ function generatePassword() {
   //ask user if special
   var confSpec = confirm("Would you like to include $pec!@l characters to your password?");
   if (confSpec) {
-    confirm("Are you sure you'd like to add $pec!@l characters??");
+    // confirm("Are you sure you'd like to add $pec!@l characters??");
     userPassPool = userPassPool + symb;
     tempPass += symb[Math.floor(Math.random() * symb.length)];
     console.log(tempPass);
@@ -57,20 +57,20 @@ function generatePassword() {
     var finalPass = userPassPool[Math.floor(Math.random() * userPassPool.length)];
     tempPass += finalPass;
     }
-      function shuffle() {
-        var tempArr = tempPass.split('');
-          for (var i = 0; i < tempArr.length-1 ; i++);{
-            var x = Math.floor(Math.random() * tempArr.length);
 
-            var temp = tempArr[i];
-            tempArr[i] = tempArr[x];
-            tempArr[x] = temp;
-          }
-          tempPass = tempArr.join('');
-          return tempPass;
+      function shuffle() {
+        var tempArr = tempPass.split("");
+          for (let i = tempArr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = tempArr[i];
+            tempArr[i] = tempArr[j];
+            tempArr[j] = temp;
+        };
+        return tempArr.join("");
       }
-      shuffle();
-    return tempPass;
+      const result = shuffle();
+
+    return result;
     
 }
 
