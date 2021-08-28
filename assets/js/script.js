67498a762844840aa17e988a2e02b52940bac5ce
Ernesto Valecillos <evalecillos@gmail.com>
2021-08-27 22:39:33 -0500

    finished the function MVP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,56 @@
 // Assignment code here
 var lowerC = "qazwsxedcrfvtgbyhnujmikolp";
-var upperC = lowerC.toUpperCase();
+var upperC = "QAZWSXEDCRFVTGBYHNUJMIKOLP";
 var symb = " !$%()&*+,-./:;<=>?@[]^_`{}|~#";
 var numb = "1234567890";
 
 function generatePassword() {
+  // ask user for desired length.
+  var lengthU = prompt("How long would you like your password to be? Please enter the number of characterS you'd like to have.\n This cannot be shorter than 8 characters, nor longer than 128 characters");
+  // set conditions for its length
+  if (lengthU < 8 || lengthU > 128 || lengthU === null || isNaN(lengthU)) {
+    alert("Please enter a valid number for the range of characters (8-128)");
+    generatePassword();
+  }
+  var userPassPool = "";
+// ask user if lowercase
+  var confLower = confirm("Would you like to include lowercase characters to your password?");
+  if (confLower) {
+    confirm("Are you sure you'd like to add lowercase characters??");
+    userPassPool = userPassPool + lowerC;
+    // console.log(userPassPool);
+  }
+//ask user if uppercase
+  var confUpper = confirm("Would you like to include UPPERCASE characters to your password?");
+  if (confUpper) {
+    confirm("Are you sure you'd like to add UPPERCASE characters??");
+    userPassPool = userPassPool + upperC;
+    // console.log(userPassPool);
+
+  }
+  //ask user if numbers
+  var confNume = confirm("Would you like to include num3r1c characters to your password?");
+  if (confNume) {
+    confirm("Are you sure you'd like to add num3r1c characters??");
+    userPassPool = userPassPool + numb;
+    // console.log(userPassPool);
+
+  }
+  //ask user if special
+  var confSpec = confirm("Would you like to include $pec!@l characters to your password?");
+  if (confSpec) {
+    confirm("Are you sure you'd like to add $pec!@l characters??");
+    userPassPool = userPassPool + symb;
+    // console.log(userPassPool);
+  }
+  var tempPass="";
+  for (var i = 0; i < lengthU ; i++) {
+    var finalPass = userPassPool[Math.floor(Math.random() * userPassPool.length)];
+    tempPass += finalPass;
+    console.log(tempPass);
+    console.log(userPassPool);
+  } return tempPass;
+  
 
 }
 
